1ed23fc6621c4cb09ac1b4860593e1f725f8ad06
Tan Wang <timmy940408@me.com>
2016-06-24 11:35:05 +0800

    Finished parsing JSON from movieDB.

--- app/src/main/java/com/tanwang9408/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/tanwang9408/popularmovies/MainActivityFragment.java
@@ -1,10 +1,18 @@
 package com.tanwang9408.popularmovies;
 
+import android.graphics.Movie;
 import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.BoolRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,6 +22,15 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,13 +40,21 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
+    public static final String APPID="878337c301e790447564e6a9915721e5";
+
+    // For popular: http://api.themoviedb.org/3/movie/popular?api_key=878337c301e790447564e6a9915721e5
+    // For top rated: http://api.themoviedb.org/3/movie/top_rated?api_key=878337c301e790447564e6a9915721e5
+
     private ArrayAdapter<String> mMovieAdapter;
+
+    private MovieInfo[] mMovieInfo;
 
     public MainActivityFragment() {
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         super.onCreate(savedInstanceState);
 
     }
@@ -39,28 +64,154 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView=inflater.inflate(R.layout.fragment_main, container, false);
-        String[] imageUrlArray={
-                "Movie 1",
-                "Movie 2",
-                "Movie 3",
-                "Movie 4",
-                "Movie 5",
-                "Movie 6",
-                "Movie 7",
-                "Movie 8",
-                "Movie 9"
 
-
-
-        };
-        List<String> imageUrls=new ArrayList<String> (Arrays.asList(imageUrlArray));
+        List<String> imageUrls=new ArrayList<String> ();
         GridView gridView=(GridView)rootView.findViewById(R.id.gridView_movies);
         mMovieAdapter=new ArrayAdapter<String>(getActivity(),R.layout.grid_item_movie
-        ,R.id.grid_item_movie_imageview,imageUrlArray);
+        ,R.id.grid_item_movie_imageview,imageUrls);
         gridView.setAdapter(mMovieAdapter);
-
+        refresh();
 
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.movie_fragment, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh(){
+        FetchMovieTask fetch=new FetchMovieTask();
+        fetch.execute(true);
+    }
+
+    // Since there is only two options for the movie list in this project, I would use boolean to indicate user choice
+    // true=popular, false=top_rated
+    public class FetchMovieTask extends AsyncTask<Boolean,Void,MovieInfo[]>{
+        private final String LOG_TAG=FetchMovieTask.class.getSimpleName();
+
+        @Override
+        protected void onPostExecute(MovieInfo[] movieInfos) {
+            super.onPostExecute(movieInfos);
+            mMovieInfo= movieInfos;
+            mMovieAdapter.clear();
+            for(MovieInfo info : movieInfos){
+                mMovieAdapter.add(info.title);
+            }
+        }
+
+        @Override
+        protected MovieInfo[] doInBackground(Boolean... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url;
+                if(params[0]) {
+                    Uri uri= Uri.parse("http://api.themoviedb.org/3/movie/popular?").buildUpon().
+                            appendQueryParameter("api_key",APPID).build();
+                    url = new URL(uri.toString());
+                }
+                else{
+                    Uri uri= Uri.parse("http://api.themoviedb.org/3/movie/top_rated?").buildUpon().
+                            appendQueryParameter("api_key",APPID).build();
+                    url = new URL(uri.toString());
+                }
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+                JSONObject jo=new JSONObject(forecastJsonStr);
+                int arrLength=jo.getJSONArray("results").length();
+                MovieInfo[] results=new MovieInfo[arrLength];
+
+                for(int i=0; i<arrLength;i++) {
+                    results[i]=new MovieInfo();
+
+
+                    results[i].title = jo.getJSONArray("results").getJSONObject(i).getString("title");
+
+
+                    results[i].imgUrl = jo.getJSONArray("results").getJSONObject(i).getString("poster_path");
+
+                    results[i].plot = jo.getJSONArray("results").getJSONObject(i).getString("overview");
+
+                    results[i].rating = jo.getJSONArray("results").getJSONObject(i).getDouble("vote_average");
+
+                    results[i].date = jo.getJSONArray("results").getJSONObject(i).getString("release_date");
+                }
+
+                return results;
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                //return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/tanwang9408/popularmovies/MovieInfo.java
@@ -0,0 +1,18 @@
+package com.tanwang9408.popularmovies;
+
+/**
+ * Created by tanwang on 6/24/16.
+ */
+public class MovieInfo {
+
+    public String title="";
+
+
+    public String imgUrl="";
+
+    public String plot="";
+
+    public double rating=0.0;
+
+    public String date="2016-7-1";
+}
