a8c111fca3405362239bc97eb67d7b751f5ccc98
Tan Wang <timmy940408@me.com>
2016-07-16 13:46:44 +0800

    Finished UI upgrade

--- app/src/main/java/com/tanwang9408/popularmovies/DetailActivity.java
+++ app/src/main/java/com/tanwang9408/popularmovies/DetailActivity.java
@@ -16,8 +16,12 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailActivityFragment())
+                    .commit();
+        }
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
