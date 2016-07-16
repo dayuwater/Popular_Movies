20f55f0eaeed775bf02e68561fa0145276382fd4
Tan Wang <timmy940408@me.com>
2016-07-16 13:42:05 +0800

    The problem in main activity UI is fixed

--- app/src/main/java/com/tanwang9408/popularmovies/MainActivity.java
+++ app/src/main/java/com/tanwang9408/popularmovies/MainActivity.java
@@ -27,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         Stetho.initializeWithDefaults(this);
         mSortOrder = Utility.getPreferredCriteria(this);
 
